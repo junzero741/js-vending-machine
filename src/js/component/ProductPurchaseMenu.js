35f4c2d06940883c1209e1492b861551e4df4a54
junzero741 <junzero741@gmail.com>
2022-08-15 22:13:00 +0900

    [STYLE] : 모든 페이지에 스타일시트 연동

--- src/js/component/ProductPurchaseMenu.js
+++ src/js/component/ProductPurchaseMenu.js
@@ -4,7 +4,12 @@ export class ProductPurchaseMenu extends HTMLElement {
 
 		const template = document.querySelector('#product-purchase-menu').content.cloneNode(true)
 		const shadow = this.attachShadow({ mode: 'open' })
+		const linkElem = document.createElement('link')
+
+		linkElem.setAttribute('rel', 'stylesheet')
+		linkElem.setAttribute('href', 'index.css')
 
 		shadow.appendChild(template)
+		shadow.appendChild(linkElem)
 	}
 }
