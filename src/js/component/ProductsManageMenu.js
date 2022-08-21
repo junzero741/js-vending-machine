f987ed7f62014842ed8323b45ccc705588bd5b25
junzero741 <junzero741@gmail.com>
2022-08-22 01:31:43 +0900

    [FEAT] : 상품 저장 로직 작성

--- src/js/component/ProductsManageMenu.js
+++ src/js/component/ProductsManageMenu.js
@@ -1,15 +1,66 @@
 export class ProductsManageMenu extends HTMLElement {
 	constructor() {
 		super()
+		this.shadow = this.attachShadow({ mode: 'open' })
 
-		const template = document.querySelector('#products-manage-menu').content.cloneNode(true)
-		const shadow = this.attachShadow({ mode: 'open' })
-		const linkElem = document.createElement('link')
+		const $template = document.querySelector('#products-manage-menu').content.cloneNode(true)
+		const $link = document.createElement('link')
 
-		linkElem.setAttribute('rel', 'stylesheet')
-		linkElem.setAttribute('href', 'index.css')
+		$link.setAttribute('rel', 'stylesheet')
+		$link.setAttribute('href', 'index.css')
 
-		shadow.appendChild(template)
-		shadow.appendChild(linkElem)
+		this.$productAddButton = $template.querySelector('#product-add-button')
+		this.$productNameInput = $template.querySelector('#product-name-input')
+		this.$productPriceInput = $template.querySelector('#product-price-input')
+		this.$productQuantityInput = $template.querySelector('#product-quantity-input')
+
+		this.shadow.appendChild($template)
+		this.shadow.appendChild($link)
+		this.init()
+	}
+
+	init() {
+		this.$productAddButton.addEventListener('click', () => {
+			const [name, price, quantity] = [
+				this.$productNameInput.value,
+				this.$productPriceInput.value,
+				this.$productQuantityInput.value,
+			]
+			this.addTableRow(name, price, quantity)
+			this.saveProduct(name, price, quantity)
+		})
+	}
+
+	addTableRow(name, price, quantity) {
+		const $container = this.shadow.querySelector('#product-inventory-container')
+		const $row = document.createElement('tr')
+		const $productName = document.createElement('td')
+		const $productPrice = document.createElement('td')
+		const $productQuantity = document.createElement('td')
+
+		$row.classList.add('product-inventory')
+		$productName.textContent = name
+		$productPrice.textContent = price
+		$productQuantity.textContent = quantity
+
+		$container.append($row)
+		$row.append($productName)
+		$row.append($productPrice)
+		$row.append($productQuantity)
+	}
+
+	saveProduct(name, price, quantity) {
+		const STORAGE_KEY = 'products'
+		const productObj = { name, price, quantity }
+		const products = JSON.parse(localStorage.getItem(STORAGE_KEY)) || []
+		const productIndex = products.findIndex((el) => el.name === name)
+
+		if (productIndex !== -1) {
+			products[productIndex] = productObj
+		} else {
+			products.push(productObj)
+		}
+
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(products))
 	}
 }
